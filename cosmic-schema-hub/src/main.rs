e9f96df1e5ec87b48300531b49c1d3cfb84b1f6c
dipghoshraj <dipghoshraj@gmail.com>
2025-01-17 22:47:33 +0530

    feat : added the create and drop db

--- cosmic-schema-hub/src/main.rs
+++ cosmic-schema-hub/src/main.rs
@@ -1,34 +1,44 @@
 mod config;
 mod db;
-use clap::{Arg, Command};
+use clap::{Parser, Subcommand};
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    cmd: Commands
+}
+
+#[derive(Subcommand, Debug, Clone)]
+enum Commands {
+    Create{
+        db_name: String,
+    },
+
+    Drop{
+        db_name: String,
+    }
+}
+
+
+
 
 fn main() {
     config::load_config();
     let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let mut client = db::database_connection(&db_url).expect("Failed to connect to database");
+    let mut client: postgres::Client = db::database_connection(&db_url).expect("Failed to connect to database");
 
-    let matches = Command::new("cosmic-schema-hub")
-        .version("0.1")
-        .author("Your Name")
-        .about("cosmic schema migrator for AFM")
-        .subcommand(
-            Command::new("create-db")
-                .about("Create the database")
-        ).subcommand(
-            Command::new("drop-db")
-                .about("Drop the database")
-        ).subcommand(
-            Command::new("migrate")
-                .about("Apply migrations")
-                .arg(
-                    Arg::new("folders")
-                        .long("folders")
-                        .help("Migration folders to apply"),
-                )
-        )
-        .get_matches();
 
-    if let Some(_) = matches.subcommand_matches("create-db") {
-        db::create_database(&mut client, "your_database_name").expect("Failed to create database");
+    let args = Args::parse();
+
+    match args.cmd {
+
+        Commands::Create{db_name} => {
+            println!("Creating database: {}", db_name);
+            db::create_database(&mut client, db_name).expect("Failed to create database");
+        },
+        Commands::Drop { db_name } => {
+            println!("Droping database: {}", db_name);
+            db::drop_database(&mut client, db_name).expect("Failed to drop database");
+        }
     }
 }
