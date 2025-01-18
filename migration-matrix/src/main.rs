5bdb40c787903751efdf8af3bded919f108525d9
dipghoshraj <dipghoshraj@gmail.com>
2025-01-18 14:38:02 +0530

    feat : initializing the the CLI

--- migration-matrix/src/main.rs
+++ migration-matrix/src/main.rs
@@ -1,3 +1,25 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    cmd: Commands
+}
+
+#[derive(Subcommand, Debug, Clone)]
+enum Commands {
+    Create{
+        db_name: String,
+    },
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    match args.cmd {
+        Commands::Create{db_name} =>{
+            println!("Creating database {}", db_name);
+        }
+    }
 }
