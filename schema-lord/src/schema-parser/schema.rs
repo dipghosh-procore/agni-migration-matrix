0dcefb54788372f66060a56116f8af5efeb42dda
dipghoshraj <dipghoshraj@gmail.com>
2025-01-25 20:02:00 +0530

    feat : schema seralizer added

---
+++ schema-lord/src/schema-parser/schema.rs
@@ -0,0 +1,16 @@
+use serde::Deserialize;
+use crate::schema::table::Table;
+use std::fs;
+
+#[derive(Debug, Deserialize)]
+pub struct Schema {
+    pub tables: Vec<Table>,
+}
+
+impl Schema {
+    pub fn parse_schema(file_path: &str) -> Result<Self, Box<dyn std::error::Error>> {
+        let file_content = fs::read_to_string(file_path)?;
+        let schema: Self = serde_yaml::from_str(&file_content)?;
+        Ok(schema)
+    }
+}
