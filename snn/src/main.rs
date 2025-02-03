46365c6f27dee3fcda2fb456bd96bd9f0afe33b5
dipghoshraj <dipghoshraj@gmail.com>
2025-02-03 23:59:39 +0530

    feat : added the tensor trnasfrom operation for matrix

--- snn/src/main.rs
+++ snn/src/main.rs
@@ -2,11 +2,17 @@ mod tensor;
 use tensor::Tensor;
 
 fn main() {
-    let tensor_a = Tensor::new(vec![1.0, 2.0, 3.0,4.0], vec![4, 1]);
-    let mut tenssor_r = Tensor::random(vec![3,3,3]);
+    let mut tensor_a = Tensor::new(vec![vec![1.0, 2.0, 3.0,4.0]], vec![1, 4]);
+    let tenssor_r = Tensor::random(vec![3,3]);
     println!("{:?} \n", tensor_a);
+    println!("{:?} \n", tensor_a.shape());
+    println!("{:?} \n", tensor_a.data());
+    println!("{:?} \n", tensor_a.data_mutable());
+
+    let tensor_a_t = tensor_a.transpose();
+    println!("{:?} \n", tensor_a_t.shape());
+    println!("{:?} \n", tensor_a_t.data());
+
+
     println!("{:?} \n", tenssor_r);
-    println!("{:?} \n", tenssor_r.shape());
-    println!("{:?} \n", tenssor_r.data());
-    println!("{:?} \n", tenssor_r.data_mutable());
 }
--- snn/src/tensor.rs
+++ snn/src/tensor.rs
@@ -3,23 +3,27 @@ use rand::Rng;
 
 #[derive(Debug)]
 pub struct Tensor {
-    data: Vec<f32>,
+    data: Vec<Vec<f32>>,
     shape: Vec<usize>
 }
 
 impl Tensor {
 
-    pub fn new(data: Vec<f32>, shape: Vec<usize>) -> Self{
-        assert_eq!(data.len(), shape.iter().product(), "Data length must match shape");
+    pub fn new(data: Vec<Vec<f32>>, shape: Vec<usize>) -> Self{
         Tensor {data: data, shape: shape}
     }
 
     pub fn random(shape: Vec<usize>) -> Self {
-        let shape_size =  shape.iter().product();
-        let mut data = Vec::with_capacity(shape_size);
+        let column_size = shape[1];
+        let row_size = shape[0];
 
-        for _ in 0..shape_size {
-            data.push(rand::rng().random::<f32>());
+        let mut data = Vec::with_capacity(column_size);
+        for _ in 0..column_size {
+            let mut row_data = Vec::with_capacity(row_size);
+            for _ in 0..row_size {
+                row_data.push(rand::rng().random::<f32>());
+            }
+            data.push(row_data);
         }
         Tensor {data: data, shape: shape}
     }
@@ -28,11 +32,22 @@ impl Tensor {
         &self.shape
     }
 
-    pub fn data(&self) -> &[f32] {
+    pub fn data(&self) -> &Vec<Vec<f32>> {
         &self.data
     }
 
-    pub fn data_mutable(&mut self) -> &mut [f32] {
+    pub fn data_mutable(&mut self) -> &mut Vec<Vec<f32>>{
         &mut self.data
+    }
+
+    pub fn transpose(&self) -> Self {
+        let (rows, cols) = (self.shape[0], self.shape[1]);
+        let mut transpose_data = vec![vec![self.data[0][0]; rows]; cols];
+        for i in 0..rows {
+            for j in 0..cols {
+                transpose_data[j][i] = self.data[i][j];
+            }
+        }
+        Tensor::new(transpose_data, vec![cols, rows])
     }
 }   
