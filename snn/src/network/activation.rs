99f368628517c2cbca888f1d7ec196a1b6773d00
dipghoshraj <dipghoshraj@gmail.com>
2025-02-06 20:51:48 +0530

    feat : activation function added

--- snn/src/network/activation.rs
+++ snn/src/network/activation.rs
@@ -2,31 +2,29 @@
 use crate::tensor::Tensor;
 use std::f64::consts::E;
 
-fn relu(input: Tensor)-> Tensor{
-    let (row, col) = input.shape();
-    let mut data = vec![vec![0; row]; col];
+pub struct Activition {
+    input: Tensor
+}
 
-    for i in 0..row{
-        for j in 0..col{
-            data[i][j] = max(0, input.data[i][j]);
-        }
+impl Activation {
+
+    fn relu(input: Tensor)-> Tensor{
+        let (row, col) = (input.shape()[0], input.shape()[1]);
+        let mut data = vec![vec![0; row]; col];
+
+        (0..row).for_each(|i| (0..col).for_each(|j| data[i][j] = max(0, input.data[i][j]) ));
+        Tensor::new(data, [row, col])
     }
-    Tensor::new(data, [row, col])
-}
 
-
-fn sigmoid(x: f32) -> &f32{
-    1.0/(1 + E.powf(-(x)))
-}
-
-fn sigmoidActivition(input: Tensor) -> Tensor{
-
-    let (row, col) = input.shape();
-    let mut data = vec![vec![0; row]; col];
-
-    for i in 0..row{
-        for j in 0..col{
-            data[i][j] = sigmoid(input[i][j])
-        }
+    fn sigma(x: f32) -> &f32{
+        1.0/(1 + E.powf(-(x)))
+    }
+    
+    fn sigmoid(input: Tensor) -> Tensor{
+    
+        let (row, col) = (input.shape()[0], input.shape()[1]);
+        let mut data = vec![vec![0; row]; col];
+        (0..row).for_each(|i| (0..col).for_each(|j| data[i][j] = sigma(input[i][j]) ));
+        Tensor::new(data, [row, col])
     }
 }
