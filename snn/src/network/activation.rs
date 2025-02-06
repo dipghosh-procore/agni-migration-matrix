3a9068b43894f0fffbc8399f56ed92bb2204d50b
dipghoshraj <dipghoshraj@gmail.com>
2025-02-07 00:03:36 +0530

    fix : layers has been updated

--- snn/src/network/activation.rs
+++ snn/src/network/activation.rs
@@ -2,33 +2,26 @@
 use crate::tensor::Tensor;
 use std::f32::consts::E;
 
-pub struct Activation {
-    input: Tensor
+pub fn relu(mut input: Tensor)-> Tensor{
+    let (row, col) = (input.shape()[0], input.shape()[1]);
+    let data_points =  input.data_mutable();
+    let mut data = vec![vec![0.0; row]; col];
+
+    (0..row).for_each(|i| (0..col).for_each(|j| data[i][j] = f32::max(0.0, data_points[i][j]) ));
+    Tensor::new(data, vec![row, col])
 }
 
-impl Activation {
+fn sigma(x: f32) -> f32 {
+    1.0f32 / (1.0f32  + E.powf(-x)) // No need for f32 suffixes here
+}
 
-    fn relu(mut input: Tensor)-> Tensor{
-        let (row, col) = (input.shape()[0], input.shape()[1]);
-        let data_points =  input.data_mutable();
-        let mut data = vec![vec![0.0; row]; col];
+pub fn sigmoid(mut input: Tensor) -> Tensor{
 
-        (0..row).for_each(|i| (0..col).for_each(|j| data[i][j] = f32::max(0.0, data_points[i][j]) ));
-        Tensor::new(data, vec![row, col])
-    }
-
-    fn sigma(x: f32) -> f32 {
-        1.0f32 / (1.0f32  + E.powf(-x)) // No need for f32 suffixes here
-    }
-    
-    fn sigmoid(mut input: Tensor) -> Tensor{
-    
-        let (row, col) = (input.shape()[0], input.shape()[1]);
-        let data_points =  input.data_mutable();
-        let mut data = vec![vec![0.0; row]; col];
+    let (row, col) = (input.shape()[0], input.shape()[1]);
+    let data_points =  input.data_mutable();
+    let mut data = vec![vec![0.0; row]; col];
 
 
-        (0..row).for_each(|i| (0..col).for_each(|j| data[i][j] = Self::sigma(data_points[i][j]) ));
-        Tensor::new(data, vec![row, col])
-    }
+    (0..row).for_each(|i| (0..col).for_each(|j| data[i][j] = sigma(data_points[i][j]) ));
+    Tensor::new(data, vec![row, col])
 }
