aaa37192e1b1dee234269723d97be1e4e17ed519
dipghoshraj <dipghoshraj@gmail.com>
2025-02-06 23:29:29 +0530

    fix : activation function

--- snn/src/network/activation.rs
+++ snn/src/network/activation.rs
@@ -1,30 +1,34 @@
 
 use crate::tensor::Tensor;
-use std::f64::consts::E;
+use std::f32::consts::E;
 
-pub struct Activition {
+pub struct Activation {
     input: Tensor
 }
 
 impl Activation {
 
-    fn relu(input: Tensor)-> Tensor{
+    fn relu(mut input: Tensor)-> Tensor{
         let (row, col) = (input.shape()[0], input.shape()[1]);
-        let mut data = vec![vec![0; row]; col];
+        let data_points =  input.data_mutable();
+        let mut data = vec![vec![0.0; row]; col];
 
-        (0..row).for_each(|i| (0..col).for_each(|j| data[i][j] = max(0, input.data[i][j]) ));
-        Tensor::new(data, [row, col])
+        (0..row).for_each(|i| (0..col).for_each(|j| data[i][j] = f32::max(0.0, data_points[i][j]) ));
+        Tensor::new(data, vec![row, col])
     }
 
-    fn sigma(x: f32) -> &f32{
-        1.0/(1 + E.powf(-(x)))
+    fn sigma(x: f32) -> f32 {
+        1.0f32 / (1.0f32  + E.powf(-x)) // No need for f32 suffixes here
     }
     
-    fn sigmoid(input: Tensor) -> Tensor{
+    fn sigmoid(mut input: Tensor) -> Tensor{
     
         let (row, col) = (input.shape()[0], input.shape()[1]);
-        let mut data = vec![vec![0; row]; col];
-        (0..row).for_each(|i| (0..col).for_each(|j| data[i][j] = sigma(input[i][j]) ));
-        Tensor::new(data, [row, col])
+        let data_points =  input.data_mutable();
+        let mut data = vec![vec![0.0; row]; col];
+
+
+        (0..row).for_each(|i| (0..col).for_each(|j| data[i][j] = Self::sigma(data_points[i][j]) ));
+        Tensor::new(data, vec![row, col])
     }
 }
